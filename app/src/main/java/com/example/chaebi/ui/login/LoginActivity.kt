e1e754ef885d1409ac769f4d14f922c4d25aa203
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 23:58:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/chaebi/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chaebi.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chaebi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
